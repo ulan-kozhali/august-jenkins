2030e3d9045a3f86d17ae1ae9e66c7143fcb70d1
ulan-kozhali <ulan.kozhaliev@gmail.com>
2025-01-09 20:43:29 -0600

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ region = "us-east-2"
 
 resource "aws_instance" "web" {
 ami = "ami-0d7ae6a161c5c4239"
-instance_type = "t2.micro"
-availability_zone = "us-east-2a"
+instance_type = var.type
+availability_zone = var.az
 key_name = "my_laptop_key"
 }
